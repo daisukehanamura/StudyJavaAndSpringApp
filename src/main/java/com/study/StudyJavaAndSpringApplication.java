4431e6f216b04951263bf13b01d55e931e1e89ed
daisukehanamura <yuu_dai_suke@yahoo.co.jp>
2025-01-02 19:24:55 +0900

    Happy New Year 2025

---
+++ src/main/java/com/study/StudyJavaAndSpringApplication.java
@@ -0,0 +1,14 @@
+package com.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyJavaAndSpringApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(StudyJavaAndSpringApplication.class, args);
+		System.out.print("HelloWorld");
+	}
+}
