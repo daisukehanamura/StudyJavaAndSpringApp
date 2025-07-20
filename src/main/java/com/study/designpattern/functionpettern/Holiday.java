e6bc66d38f9d248c5248b82055a351a99fe528cf
dellgreen <fousenh2@gmail.com>
2025-07-20 18:29:20 +0900

    add

---
+++ src/main/java/com/study/designpattern/functionpettern/Holiday.java
@@ -0,0 +1,20 @@
+package com.study.designpattern.functionpettern;
+
+import java.util.function.Consumer;
+
+public class Holiday {
+    private final String holidayName;
+
+    Holiday(String holidayName) {
+        this.holidayName = holidayName;
+    }
+
+    public void callHoliday(Consumer<String> holidayFunction) {
+        holidayFunction.accept(holidayName);
+    }
+
+    public void callHolidayname(Consumer<String> holidayNameFunction) {
+        holidayNameFunction.accept(holidayName);
+    }
+
+}
